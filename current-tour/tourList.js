361de61fff063c33c89a3643209f5b3a7ef18b50
ngohoangviet0710 <ngohoangviet0710@gmail.com>
2026-08-22 22:13:47 +0700

    init commit

---
+++ current-tour/tourList.js
@@ -0,0 +1,81 @@
+const tourList = [
+  {
+    name: "SG - Mỹ Tho - Bến Tre",
+    duration: "1 ngày",
+    price: 500000,
+    departureDateTime: null,
+    hotelStars: null,
+    departurePoint: "TP. Hồ Chí Minh",
+    departureSchedule: "Mỗi ngày",
+    type: "Ghép đoàn",
+    image: "my-tho.jpg"
+  },
+  {
+    name: "SG - Cồn Chim (Trà Vinh)",
+    duration: "1 ngày",
+    price: 990000,
+    departureDateTime: null,
+    hotelStars: null,
+    departurePoint: "TP. Hồ Chí Minh",
+    departureSchedule: "Thứ 7 và Chủ nhật hàng tuần",
+    type: "Ghép đoàn",
+    note: "Giá tùy số lượng khách",
+    image: "con-chim.jpg"
+  },
+  {
+    name: "SG - Trà Vinh - Bảo Tàng Dừa Sáp",
+    duration: "1 ngày",
+    price: 850000,
+    departureDateTime: null,
+    hotelStars: null,
+    departurePoint: "TP. Hồ Chí Minh",
+    departureSchedule: "Chủ nhật hàng tuần",
+    type: "Ghép đoàn",
+    note: "Giá tùy số lượng khách",
+    image: "bao-tang.jpg"
+  },
+  {
+    name: "SG - Mỹ Tho - Bến Tre - Cần Thơ",
+    duration: "2 ngày 1 đêm",
+    price: 1600000,
+    departureDateTime: null,
+    hotelStars: null,
+    departurePoint: "TP. Hồ Chí Minh",
+    departureSchedule: "Mỗi ngày",
+    type: "Ghép đoàn",
+    image: "cho-noi.jpg"
+  },
+  {
+    name: "SG - Cần Thơ - Chợ nổi Cái Răng - Cồn Sơn",
+    duration: "2 ngày 1 đêm",
+    price: 2190000,
+    departureDateTime: null,
+    hotelStars: null,
+    departurePoint: "TP. Hồ Chí Minh",
+    departureSchedule: "Thứ 2, 4, 6, 7 hàng tuần",
+    type: "Ghép đoàn",
+    image: "cheo-suong.jpg"
+  },
+  {
+    name: "Mỹ Tho - Bến Tre - Cần Thơ - Cà Mau - Bạc Liêu - Sóc Trăng",
+    duration: "3 ngày 2 đêm",
+    price: 3080000,
+    departureDateTime: null,
+    hotelStars: null,
+    departurePoint: "TP. Hồ Chí Minh",
+    departureSchedule: "Thứ 3 và Thứ 6 hàng tuần",
+    type: "Ghép đoàn",
+    image: "mui-ca-mau.jpg"
+  },
+  {
+    name: "Mỹ Tho - Bến Tre - Châu Đốc - Chùa Bà - Rừng Tràm Trà Sư - Cần Thơ - Cà Mau - Bạc Liêu - Sóc Trăng",
+    duration: "4 ngày 3 đêm",
+    price: 4380000,
+    departureDateTime: null,
+    hotelStars: null,
+    departurePoint: "TP. Hồ Chí Minh",
+    departureSchedule: "Thứ 2 và Thứ 5 hàng tuần",
+    type: "Ghép đoàn",
+    image: "rung-tram.jpg"
+  }
+];
